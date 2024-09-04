63d440acdc64b5c17093ccca7c91b572e31fc61f
Munoz <mimunoz3@cps.k12.il.us>
2024-09-04 09:29:27 -0500

    first commit

---
+++ python_basics/lesson1.py
@@ -0,0 +1,3 @@
+print("my name is Mayra")
+print("I am  junior")
+print("my birthday is December 29")
---
+++ week2.py
@@ -0,0 +1,10 @@
+# ask someone what day it is
+day = input("What day is it?")
+print ("Today is" + day)
+# ask them for their favorite holiday
+holiday = input("What is your favorite holiday?")
+print ("Their favorite holiday is" + holiday )
+#print out the day and their favorite holiday in
+# a sentence
+print ("Today is" + day + " and their favorite holiday is" + holiday)
+       
