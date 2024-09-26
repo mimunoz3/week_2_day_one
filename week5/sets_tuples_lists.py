82678ad5b62fc81f0e38a7cd713ef9356296878f
Munoz <mimunoz3@cps.k12.il.us>
2024-09-26 08:51:40 -0500

    changes worked on

--- week5/sets_tuples_lists.py
+++ week5/sets_tuples_lists.py
@@ -20,5 +20,36 @@ fruits =["apple", "orange", "banana", "coconut", "kiwi", "strawberry"]
 # fruits.clear()
 
 # print(fruits[0:3])
-for fruit in fruits:
-print(fruit)
+# for fruit in fruits:
+# print(fruit)
+
+cars = ["bmw", "maserati", "audi", "mercedes", "ferrari"]
+print(f"these are list of {cars}")
+print(f"the first car is {cars[0]}")
+
+# changing the value of the list
+cars[0] = "toyota"
+print(f"is the last car is {cars[-1]}")
+cars[-1] = "lamborghini"
+print(f"the last car is {cars[-1]}")
+
+#adding a new value to the list
+cars.append("bugatti")
+print(cars)
+cars.remove("maserati")
+print(cars)
+
+# looping through the list otherwise called iterating through the list
+
+#for car in cars:
+#print(len(car))
+#print(car)
+
+#carRequest = input("add a new car please: ")
+#cars.append(carRequest)
+#print(cars)
+#print(len(cars))
+#print(cars.upper())
+#print(cars)
+#if len(cars) > 10:
+ #   break
