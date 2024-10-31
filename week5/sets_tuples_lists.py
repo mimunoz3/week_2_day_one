8963ddcfbb8080740118e221ed770b1d5d5e1896
Munoz <mimunoz3@cps.k12.il.us>
2024-10-31 09:23:22 -0500

    changes worked on

--- week5/sets_tuples_lists.py
+++ week5/sets_tuples_lists.py
@@ -1,14 +1,69 @@
 # collection =single "variable" used to store multiple values
+
 # list = [] ordered and changeab;e, dublicates OK
-# set = {} unordered and immutable, but Add/Remove OK. NO dublicates
-# tuple = () ordered and unchangeable. Duplicates OK. FASTER
-
-fruits =["apple", "orange", "banana", "coconut", "kiwi", "strawberry"]
-
+# fruits =["apple", "orange", "banana", "coconut", "kiwi", "strawberry"]
 # print(dir(fruits))
 #print(help(fruits))
 # print(len(fruits))
 # print("pineapple" in fruits)
+
+# set = {} unordered and immutable, but Add/Remove OK. NO dublicates
+#fruits = {"apple", "orange", "banana", "coconut"}
+#print(fruits)
+#print(dir(fruits))
+#print(len(fruits))
+#promt("pineapple" in fruits)
+# print(fruits[0]) - gives an error because a set has no order, it is not a list
+#fruits.add("pineapple")
+#fruits.add("strawberry")
+#fruits.remove("apple")
+#fruits.pop()
+#fruits.clear()
+#print(fruits)
+
+# tuple = () ordered and unchangeable. Duplicates OK. FASTER
+#fruits = ("apple", "orange", "banana", "coconut")
+#print(dir(fruits))
+#print(help(fruits))
+#print(len(fruits))
+#print("pineapple" in fruits)
+#print(fruits.index("apple"))
+#print(fruits.count("cocounut"))
+#print(fruits)
+#for fruit in fruits:
+#    print(fruit)
+
+# dictionary - a collection of {key:value} pairs ordered and changeable. No duplicates
+# capitals = {"USA":"Washington D.C.",
+#            "India": "New Delhi",
+#            "China": "Beijing",
+#            "Russia": "Moscow"}
+##print(dir(capitals))
+#print(help(capitals))
+#print(capitals.get("USA"))
+#if capitals.get("India"):
+    #print("That capital exists")
+#else:
+#    print("That capital doesn't exist")
+#capitals.update({"Germany": "Berlin"})
+#capitals.update({"USA": "Detroit"})
+#print(capitals)
+#capitals.pop("China")
+#capitals.popitem()
+#capitals.clear
+
+#keys = capitals.keys()
+#print(keys)
+#for key in capitals.keys():
+#    print()
+
+#values = capitals.values()
+#for value in capitals.values():
+#   print(value)
+
+#items = capitals.items()
+#for key, value in capitals.items():
+#    print(f"{key}: {value}")
 
 # you can reassign value using this:
 # fruits[1] = "pineapple"
@@ -51,34 +106,34 @@ fruits =["apple", "orange", "banana", "coconut", "kiwi", "strawberry"]
 #print(len(cars))
 #print(cars.upper())
 #print(cars)
-#if len(cars) > 10:
- #   break
+#if len(cars) > 10: 
+#break
 
 
 # challenge
 # create a list of friends 
 # make sure the initial list is 0 
-friends = []
+#friends = []
 # add a new friend to the list, add at least 5
-friends.append("lesly")
-friends.append("maggie")
-friends.append("abby")
-friends.append("Julian")
-friends.append("vivi")
-print(friends)
+#friends.append("lesly")
+#friends.append("maggie")
+#friends.append("abby")
+#friends.append("Julian")
+#friends.append("vivi")
+#print(friends)
 # remove a friend 
-friends.remove("Julian")
+#friends.remove("Julian")
 # insert a friend at a specidic index, 2
-friends.insert(2, 'Noelia')
+#friends.insert(2, 'Noelia')
 # print the list of friends 
-print(friends)
+#print(friends)
 # loop through the list and print the friends name
-for friend in friends:
-    print(len(friends))
-    print(friend)
+# for friend in friends:
+ #   print(len(friends))
+ #   print(friend)
 
 # see if a particular friend is in the list (boolean value)
-print("Xavier" in friends)
+#print("Xavier" in friends)
 # if the list is greater than 10 break the loop
 #if len(cars) > 10:
  #   break
